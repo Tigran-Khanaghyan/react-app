a09b0a26fe20b798ca91f5d2952d4713392a9230
Tigran-Khanaghyan <khanaghyan.tigran@gmail.com>
2023-04-19 01:16:56 +0400

    Create simple chat html template

--- src/chat/components/window.jsx
+++ src/chat/components/window.jsx
@@ -1,25 +1,74 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./window.css";
+import Button from "shared/Button";
 
 function Window() {
+  const messagesEndRef = useRef();
+
+  const [message, setMessage] = useState();
+  const [conversation, setConversation] = useState([]);
+
+  const sendFirstMessage = () => {
+    const newMessage = {
+      id: "user1",
+      message,
+    };
+
+    setConversation((prev) => [...prev, newMessage]);
+  };
+
+  const sendSecondMessage = () => {
+    const newMessage = {
+      id: "user2",
+      message,
+    };
+
+    setConversation((prev) => [...prev, newMessage]);
+  };
+
+  useEffect(() => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  });
+
   return (
     <div>
-      <div class="chat-popup" id="myForm">
-        <div class="form-container">
+      <div className="chat-popup" id="myForm">
+        <div className="form-container">
           <h1>Chat</h1>
-          <div className="conversation-container"></div>
+          <div className="conversation-container">
+            {conversation.map((item) => {
+              const className =
+                item.id === "user1" ? "user1-message" : "user2-message";
+              const containerclassName = item.id === "user1" ? "left" : "right";
+              return (
+                <div className={containerclassName}>
+                  <span className={className}>{item.message}</span>
+                </div>
+              );
+            })}
+            <div ref={messagesEndRef} />
+          </div>
 
-          <label for="msg">
+          <label htmlFor="msg">
             <b>Message</b>
           </label>
-          <textarea placeholder="Type message.." name="msg" required></textarea>
+          <textarea
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            placeholder="Type message.."
+            name="msg"
+            required
+          ></textarea>
 
-          <button type="submit" class="btn">
-            Send
-          </button>
-          <button type="button" class="btn cancel" onclick="closeForm()">
+          <Button type="submit" className="btn" onClick={sendFirstMessage}>
+            Send user 1
+          </Button>
+          <Button type="submit" className="btn" onClick={sendSecondMessage}>
+            Send user 2
+          </Button>
+          <Button type="button" className="btn cancel">
             Close
-          </button>
+          </Button>
         </div>
       </div>
     </div>
